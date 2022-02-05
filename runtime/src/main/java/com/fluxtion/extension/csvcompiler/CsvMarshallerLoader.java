47b36d8f49416463a7743d5e7034e0d66722957a
greg <gregphiggins@gmai.com>
2022-02-05 13:22:14 +0000

    Formatting

--- runtime/src/main/java/com/fluxtion/extension/csvcompiler/CsvMarshallerLoader.java
+++ runtime/src/main/java/com/fluxtion/extension/csvcompiler/CsvMarshallerLoader.java
@@ -29,17 +29,17 @@ public interface CsvMarshallerLoader<T> {
 
     Class<T> targetClass();
 
-    static<T> CsvMarshallerLoader<T> marshaller(Class<T> clazz){
+    CsvMarshallerLoader<T> setErrorLog(ValidationLogger errorLog);
+
+    void stream(Consumer<T> consumer, Reader in);
+
+    static <T> CsvMarshallerLoader<T> marshaller(Class<T> clazz) {
         return load(CsvMarshallerLoader.class).stream()
                 .map(Provider::get)
-                .map(obj -> (CsvMarshallerLoader<T>)obj)
+                .map(obj -> (CsvMarshallerLoader<T>) obj)
                 .filter(svc -> svc.targetClass().equals(clazz))
                 .findAny()
                 .get();
     }
-
-    CsvMarshallerLoader<T> setErrorLog(ValidationLogger errorLog);
-
-    void stream(Consumer<T> consumer, Reader in);
 
 }
